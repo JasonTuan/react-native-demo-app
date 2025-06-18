ae14349cf7a37ff5ae4ae70b363b071f386d6ba9
Jason Tuan <tuan.nguyen@bliksund.com>
2025-06-18 17:44:42 +0700

    Setup Drawer

--- README.md
+++ README.md
@@ -48,3 +48,6 @@ Join our community of developers creating universal apps.
 
 - [Expo on GitHub](https://github.com/expo/expo): View our open source platform and contribute.
 - [Discord community](https://chat.expo.dev): Chat with Expo users and ask questions.
+
+## References
+- https://medium.com/free-code-camp/how-to-build-a-nested-drawer-menu-with-react-native-a1c2fdcab6c9
--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,5 +1,27 @@
 import { Stack } from "expo-router";
+import {GestureHandlerRootView} from "react-native-gesture-handler";
+import {Drawer} from "expo-router/drawer";
 
 export default function RootLayout() {
-  return <Stack />;
+  // return <Stack />;
+  return (
+      <GestureHandlerRootView style={{ flex: 1 }}>
+        <Drawer>
+          <Drawer.Screen
+              name="index" // This is the name of the page and must match the url from root
+              options={{
+                drawerLabel: 'Home',
+                title: 'Home',
+              }}
+          />
+          <Drawer.Screen
+              name="about-us" // This is the name of the page and must match the url from root
+              options={{
+                drawerLabel: 'About Us',
+                title: 'About Us',
+              }}
+          />
+        </Drawer>
+      </GestureHandlerRootView>
+  );
 }
